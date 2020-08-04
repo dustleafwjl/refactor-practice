31934f1115cc5e5dbf6343d9d16cdf9b34ea8bd5
dustleafwjl <wangjianglin_priv@qq.com>
2020-08-04 09:56:47 +0800

    [jianglin wang] feat: refactor project 05

--- 5/src/main/java/com/twu/refactoring/Taxi.java
+++ 5/src/main/java/com/twu/refactoring/Taxi.java
@@ -4,6 +4,15 @@ public class Taxi {
     private boolean airConditioned;
     private final int totalKms;
     private final boolean peakTime;
+    private static final int FIXED_CHARGE = 50;
+    private static final double PEAK_TIME_MULTIPLIER = 1.2;
+    private static final double OFF_PEAK_MULTIPLIER = 1.0;
+    private static final int RATE_CHANGE_DISTANCE = 10;
+    private static final int PRE_RATE_CHANGE_NON_AC_RATE = 15;
+    private static final int POST_RATE_CHANGE_NON_AC_RATE = 12;
+    private static final int PRE_RATE_CHANGE_AC_RATE = 20;
+    private static final int POST_RATE_CHANGE_AC_RATE = 17;
+    private static final double SALES_TAX_RATE = 0.1;
 
     public Taxi(boolean airConditioned, int totalKms, boolean peakTime) {
         this.airConditioned = airConditioned;
@@ -21,5 +30,24 @@ public class Taxi {
 
     public boolean isPeakTime() {
         return peakTime;
+    }
+
+    public double getTotalCost() {
+        double totalCost = 0;
+
+        // fixed charges
+        totalCost += FIXED_CHARGE;
+
+        // taxi charges
+        int totalKms = getTotalKms();
+        double peakTimeMultiple = isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
+        if(isAirConditioned()) {
+            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_AC_RATE * peakTimeMultiple;
+            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_AC_RATE * peakTimeMultiple;
+        } else {
+            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
+            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
+        }
+        return totalCost * (1 + SALES_TAX_RATE);
     }
 }
