6a5c336c8ce136ee1004e9876b0fac2fef7476ff
dustleafwjl <wangjianglin_priv@qq.com>
2020-08-04 20:28:42 +0800

    [jianglin wang] feat: refactor project 06

--- 6/src/main/java/com/twu/refactoring/Customer.java
+++ 6/src/main/java/com/twu/refactoring/Customer.java
@@ -32,27 +32,17 @@ public class Customer {
 			// determine amounts for each line
 			switch (each.getMovie().getPriceCode()) {
 			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				thisAmount = getThisAmountForRegular(thisAmount, each);
 				break;
 			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
+				thisAmount = getThisAmountForNewRelease(thisAmount, each);
 				break;
 			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				thisAmount = getThisAmountForChildrens(thisAmount, each);
 				break;
 
 			}
-
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints = getFrequentRenterPoints(frequentRenterPoints, each);
 
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
@@ -62,9 +52,38 @@ public class Customer {
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-				+ " frequent renter points";
+		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return result;
+	}
+
+	private int getFrequentRenterPoints(int frequentRenterPoints, Rental each) {
+		// add frequent renter points
+		frequentRenterPoints++;
+		// add bonus for a two day new release rental
+		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& each.getDaysRented() > 1){
+			frequentRenterPoints++;
+		}
+		return frequentRenterPoints;
+	}
+
+	private double getThisAmountForChildrens(double thisAmount, Rental each) {
+		thisAmount += 1.5;
+		if (each.getDaysRented() > 3)
+			thisAmount += (each.getDaysRented() - 3) * 1.5;
+		return thisAmount;
+	}
+
+	private double getThisAmountForNewRelease(double thisAmount, Rental each) {
+		thisAmount += each.getDaysRented() * 3;
+		return thisAmount;
+	}
+
+	private double getThisAmountForRegular(double thisAmount, Rental each) {
+		thisAmount += 2;
+		if (each.getDaysRented() > 2)
+			thisAmount += (each.getDaysRented() - 2) * 1.5;
+		return thisAmount;
 	}
 
 }
